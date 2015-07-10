5a2a5d40ac4503b0d04aab3b9191b9ff00878393
nplus <junnplus@qq.com>
2015-07-10 22:00:41 +0800

    bp url

--- VJ/views/problem/__init__.py
+++ VJ/views/problem/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from .problem import ProblemListView
+from .problem import ProblemListView, ProblemDetailView
 
 bp_problem = Blueprint('problem', __name__)
 
@@ -7,5 +7,12 @@ bp_problem.add_url_rule(
     '',
     endpoint = 'problem',
     view_func = ProblemListView.as_view('problem'),
+    methods = ['get', 'post']
+)
+
+bp_problem.add_url_rule(
+    '/<origin_oj>/<problem_id>',
+    endpoint = 'detail',
+    view_func = ProblemDetailView.as_view('detail'),
     methods = ['get', 'post']
 )
