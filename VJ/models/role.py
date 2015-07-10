fd861ac6c8263760bc3e0d9285c70a064d4e99cd
nplus <junnplus@qq.com>
2015-07-10 15:53:27 +0800

    role model update

--- VJ/models/role.py
+++ VJ/models/role.py
@@ -2,7 +2,7 @@ from VJ import db
 from datetime import datetime
 from flask.ext.security import RoleMixin
 
-class Role(db.Document, RoleMixin):
+class RoleModel(db.Document, RoleMixin):
     name = db.StringField(max_length=80, unique=True)
     description = db.StringField(max_length=255)
     created_at = db.DateTimeField(default=datetime.now, required=True)
