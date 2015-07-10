8b040f562d5a7cabef2e6541f5cf36cb109798b7
nplus <junnplus@qq.com>
2015-07-10 22:00:26 +0800

    problem detail

--- VJ/views/problem/problem.py
+++ VJ/views/problem/problem.py
@@ -19,3 +19,14 @@ class ProblemListView(MethodView):
 
     def post(self):
         pass
+
+class ProblemDetailView(MethodView):
+
+    template = 'problem/problem_detail.html'
+
+    def get(self, origin_oj , problem_id):
+        problem = ProblemItem.objects.get_or_404(
+                origin_oj=origin_oj,
+                problem_id=problem_id
+            )
+        return render_template(self.template, problem=problem)
