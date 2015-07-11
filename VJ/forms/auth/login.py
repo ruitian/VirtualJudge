835b64103ab4036aff534a3f6aab0e6d2fa2c122
nplus <junnplus@qq.com>
2015-07-11 10:04:54 +0800

    update

--- VJ/forms/auth/login.py
+++ VJ/forms/auth/login.py
@@ -1,14 +1,14 @@
 #-*- coding: utf-8 -*-
 from flask_wtf import Form
-from wtforms import PasswordField, StringField, BooleanField, SubmitField
+from wtforms import PasswordField, StringField, BooleanField
 from wtforms.validators import Email, Required, ValidationError
 
 from VJ.models import UserModel
 
 class LoginForm(Form):
-    email = StringField(u'邮箱', [Required(), Email()])
-    password = PasswordField(u'密码', [Required()])
-    remember_me = BooleanField(u'记住我')
+    email = StringField('Email', [Required(), Email()])
+    password = PasswordField('Password', [Required()])
+    remember_me = BooleanField('Remember Me')
 
     def validate_password(self, field):
         user = UserModel.objects.filter(
