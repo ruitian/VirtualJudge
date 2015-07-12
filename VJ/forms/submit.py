f3b9099e7551c2b8934696daede3cf77b98203d2
nplus <junnplus@qq.com>
2015-07-12 23:32:24 +0800

    submit

--- VJ/forms/submit.py
+++ VJ/forms/submit.py
@@ -2,6 +2,8 @@ from flask_wtf import Form
 from wtforms import StringField, TextAreaField, SelectField
 from wtforms.validators import Required, ValidationError
 
+from VJ.models import SolutionItem
+
 class SubmitForm(Form):
     origin_oj = StringField('OJ', [Required()])
     problem_id = StringField('Problem ID', [Required()])
@@ -23,6 +25,15 @@ class SubmitForm(Form):
             ('haskell', 'Haskell'),
             ('python2', 'Python2'),
             ('python3', 'Python3')
-        ]
+        ],
+        validators=[Required()]
     )
     code = TextAreaField('Code', [Required()])
+
+    def submit(self):
+        return SolutionItem.create_solution(
+            origin_oj = self.origin_oj.data,
+            problem_id = self.problem_id.data,
+            language = self.language.data,
+            code = self.code.data
+        )
