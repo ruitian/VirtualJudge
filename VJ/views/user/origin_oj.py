bcf68cb13b4a9df660ea749457b061905a8c95e6
nplus <junnplus@qq.com>
2015-07-13 13:41:08 +0800

    origin oj binding and unbind

---
+++ VJ/views/user/origin_oj.py
@@ -0,0 +1,75 @@
+from flask import (
+    request,
+    redirect,
+    url_for,
+    render_template
+)
+from flask.views import MethodView
+from flask.ext.login import login_user, login_required, current_user
+
+from VJ.models import UserModel
+from VJ.forms import OriginOJAccountForm
+
+class OriginOJView(MethodView):
+
+    template = 'user/settings/origin_oj.html'
+
+    @login_required
+    def get(self):
+        form = OriginOJAccountForm()
+        return render_template(self.template, form=form)
+
+class PojView(MethodView):
+
+    @login_required
+    def get(self):
+        current_user.update(poj=None)
+        return redirect(url_for('user.origin_oj'))
+
+    @login_required
+    def post(self):
+        form = OriginOJAccountForm()
+        account = form.generate_account()
+        current_user.update(poj=account)
+        return redirect(url_for('user.origin_oj'))
+
+class HduView(MethodView):
+
+    @login_required
+    def get(self):
+        current_user.update(hdu=None)
+        return redirect(url_for('user.origin_oj'))
+
+    @login_required
+    def post(self):
+        form = OriginOJAccountForm()
+        account = form.generate_account()
+        current_user.update(hdu=account)
+        return redirect(url_for('user.origin_oj'))
+
+class SdutView(MethodView):
+
+    @login_required
+    def get(self):
+        current_user.update(sdut=None)
+        return redirect(url_for('user.origin_oj'))
+
+    @login_required
+    def post(self):
+        form = OriginOJAccountForm()
+        account = form.generate_account()
+        current_user.update(sdut=account)
+        return redirect(url_for('user.origin_oj'))
+
+class FzuView(MethodView):
+
+    @login_required
+    def get(self):
+        current_user.update(fzu=None)
+        return redirect(url_for('user.origin_oj'))
+    @login_required
+    def post(self):
+        form = OriginOJAccountForm()
+        account = form.generate_account()
+        current_user.update(fzu=account)
+        return redirect(url_for('user.origin_oj'))
