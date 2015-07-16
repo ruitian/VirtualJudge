0f887df2e2c43d5ce34b695156972e899ae04dce
nplus <junnplus@qq.com>
2015-07-16 15:33:31 +0800

    problem id v

--- VJ/forms/submit.py
+++ VJ/forms/submit.py
@@ -1,8 +1,16 @@
 from flask_wtf import Form
 from wtforms import StringField, TextAreaField, SelectField
-from wtforms.validators import Required, ValidationError
+from wtforms.validators import Required, ValidationError, Length
 
 from VJ.models import SolutionItem
+from VJ.models import ProblemItem
+
+ORIGIN_OJ = [
+    'poj',
+    'hdu',
+    'sdut',
+    'fzu'
+]
 
 class SubmitForm(Form):
     origin_oj = StringField('OJ', [Required()])
@@ -28,9 +36,16 @@ class SubmitForm(Form):
         ],
         validators=[Required()]
     )
-    code = TextAreaField('Code', [Required()])
+    code = TextAreaField('Code', [Required(), Length(min=4)])
+    def validate_origin_oj(self, field):
+        if field.data not in ORIGIN_OJ:
+            raise ValidationError('Invalid origin oj')
 
-    def submit(self):
+    def validate_problem_id(self, field):
+        if not ProblemItem.objects.filter(problem_id=field.data):
+            raise ValidationError('Invalid problem id')
+
+    def generate_solution(self):
         return SolutionItem.create_solution(
             origin_oj = self.origin_oj.data,
             problem_id = self.problem_id.data,
