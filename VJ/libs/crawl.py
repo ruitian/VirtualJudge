4ba5887b40b6e85424feef727c11672e1e820522
nplus <junnplus@qq.com>
2015-07-16 21:36:40 +0800

    class

--- VJ/libs/crawl.py
+++ VJ/libs/crawl.py
@@ -2,15 +2,83 @@ from billiard import Process
 
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
-
 from scrapy.utils.log import configure_logging
 
 settings = get_project_settings()
 
-class AccountInitCrawler():
+class Crawler():
 
     def __init__(self):
         self.crawler = CrawlerProcess(settings)
+
+class OriginOJInitCrawler():
+
+    def __init__(self):
+        self.crawler = CrawlerProcess(settings)
+
+    def _crawl(self, origin_oj):
+        self.crawler.crawl(
+            origin_oj + '_init',
+        )
+        self.crawler.start()
+        self.crawler.stop()
+
+    def crawl(self):
+        p = Process(
+            target=self._crawl,
+            args=[
+                origin_oj
+            ]
+        )
+        p.start()
+        p.join()
+
+class CodeSubmitCrawler(Crawler):
+
+    def _crawl(self,
+            origin_oj,
+            solution_id,
+            problem_id,
+            language,
+            code,
+            username,
+            password):
+        self.crawler.crawl(
+            origin_oj + '_submit',
+            solution_id = solution_id,
+            problem_id = problem_id,
+            language = language,
+            code = code,
+            username = username,
+            password = password
+        )
+        self.crawler.start()
+        self.crawler.stop()
+
+    def crawl(self,
+            origin_oj,
+            solution_id,
+            problem_id,
+            language,
+            code,
+            username,
+            password):
+        p = Process(
+            target=self._crawl,
+            args=[
+                origin_oj,
+                solution_id,
+                problem_id,
+                language,
+                code,
+                username,
+                password
+            ]
+        )
+        p.start()
+        p.join()
+
+class AccountCrawler(Crawler):
 
     def _crawl(self, origin_oj, username, password):
         self.crawler.crawl(
