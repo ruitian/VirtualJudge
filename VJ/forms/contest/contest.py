678268f498fdce8ebd15c6d9911a9b532e8e556c
nplus <junnplus@qq.com>
2015-07-27 02:04:58 +0800

    update

--- VJ/forms/contest/contest.py
+++ VJ/forms/contest/contest.py
@@ -9,11 +9,15 @@ from wtforms import (
     SubmitField,
     FormField
 )
-from wtforms.validators import Email, Required, ValidationError
+from wtforms.validators import Required, ValidationError
 from datetime import datetime
 
+from VJ.models import ContestModel, ProblemItem
+
+
 class ProblemForm(Form):
-    origin_oj = SelectField('Origin OJ',
+    origin_oj = SelectField(
+        'Origin OJ',
         choices=[
             ('poj', 'POJ'),
             ('hdu', 'HDU'),
@@ -21,16 +25,53 @@ class ProblemForm(Form):
             ('fzu', 'FZU')
         ],
     )
-    problem_id = StringField('Problem ID')
+    problem_id = StringField('Problem ID', [Required()])
     title = StringField('Title')
 
+    def validate_problem_id(self, field):
+        if not ProblemItem.objects(
+            origin_oj=self.origin_oj.data,
+            problem_id=field.data
+        ).first():
+            raise ValidationError('Problem Id is required')
+
+
 class ContestForm(Form):
-    title = StringField('Contest Title',
-        default='The king asked me to look for the mountains. ')
+    title = StringField(
+        'Contest Title',
+        [Required()],
+        default='The king asked me to look for the mountains. ',
+        )
     password = PasswordField('Password')
-    start_at = DateTimeField('Start Time', default=datetime.now)
-    end_at = DateTimeField('End Time')
+    start_at = DateTimeField(
+        'Start Time',
+        [Required()],
+        default=datetime.now,
+    )
+    end_at = DateTimeField(
+        'End Time',
+        [Required()]
+    )
     description = TextAreaField('Description')
     problems = FieldList(FormField(ProblemForm))
     remove = SubmitField()
     add = SubmitField()
+
+    def validate_end_at(self, field):
+        if field.data < self.start_at.data:
+            raise ValidationError('End time is not greater than start time')
+
+    def validate_remove(self, field):
+        pass
+
+    def validate_add(self, field):
+        pass
+
+    def generate_contest(self):
+        return ContestModel.create_contest(
+            title=self.title.data,
+            password=self.password.data,
+            start_at=self.start_at.data,
+            end_at=self.end_at.data,
+            description=self.description.data
+        )
--- VJ/views/__init__.py
+++ VJ/views/__init__.py
@@ -1,6 +1,6 @@
-from .auth import bp_auth
-from .index import bp_index
-from .problem import bp_problem
-from .contest import bp_contest
-from .solution import bp_solution
-from .user import bp_user
+from .auth import bp_auth # noqa
+from .index import bp_index # noqa
+from .problem import bp_problem # noqa
+from .contest import bp_contest # noqa
+from .solution import bp_solution # noqa
+from .user import bp_user # noqa
