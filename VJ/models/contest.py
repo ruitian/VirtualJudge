6ab9307f475b9d8ea4bfac6046c818eb2f99c4d6
nplus <junnplus@qq.com>
2015-07-27 01:16:38 +0800

    model contest

--- VJ/models/contest.py
+++ VJ/models/contest.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from VJ.models.problem import ProblemItem
 from mongoengine import DENY, NULLIFY
 
+
 class ContestModel(db.Document):
     id = db.SequenceField(primary_key=True)
     title = db.StringField()
@@ -17,3 +18,27 @@ class ContestModel(db.Document):
     start_at = db.DateTimeField(required=True)
     end_at = db.DateTimeField(required=True)
     created_at = db.DateTimeField(default=datetime.now, required=True)
+
+    meta = {
+        'collection': 'Contest',
+        'indexs': ['id']
+    }
+
+    @classmethod
+    def create_contest(
+            cls,
+            title,
+            password,
+            start_at,
+            end_at,
+            description, **kwargs):
+        print title, start_at, end_at
+        return cls.objects.create(
+            title=title,
+            password=password,
+            start_at=start_at,
+            end_at=end_at,
+            description=description,
+            **kwargs
+        )
+
