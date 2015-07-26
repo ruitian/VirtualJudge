a745cfacaf86baddb19301833169bddae7844c71
nplus <junnplus@qq.com>
2015-07-26 20:09:44 +0800

    contest model

--- VJ/models/contest.py
+++ VJ/models/contest.py
@@ -1,12 +1,19 @@
 from VJ import db
 from datetime import datetime
+from VJ.models.problem import ProblemItem
+from mongoengine import DENY, NULLIFY
 
 class ContestModel(db.Document):
     id = db.SequenceField(primary_key=True)
     title = db.StringField()
     password = db.StringField()
     description = db.StringField()
-    problems = db.ListField()
+    problems = db.ListField(
+        db.ReferenceField(
+            ProblemItem,
+            reverse_delete_rule=DENY
+        )
+    )
     start_at = db.DateTimeField(required=True)
     end_at = db.DateTimeField(required=True)
     created_at = db.DateTimeField(default=datetime.now, required=True)
