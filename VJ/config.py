9d00bd68b65da91bcf4931502fcb7dfcd6998fea
nplus <junnplus@qq.com>
2015-07-28 04:48:07 +0800

    crontab

--- VJ/config.py
+++ VJ/config.py
@@ -41,7 +41,7 @@ class Config:
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    VJ_MAIL_SUBJECT_PREFIX = '[VJ]'
+    VJ_MAIL_SUBJECT_PREFIX = '[Virtual Judge]'
     VJ_MAIL_SENDER = os.environ.get('MAIL_USERNAME')
     VJ_ADMIN = os.environ.get('VJ_ADMIN')
 
@@ -65,7 +65,22 @@ class Config:
         'test': {
             'task': 'VJ.libs.tasks.test',
             'schedule': crontab(minute='*/1'),
-            'args': (1, 2, 3)
+            'args': (1, 2)
+        },
+        'hdu': {
+            'task': 'VJ.libs.tasks.problem_crawler',
+            'schedule': crontab(minute=0, hour=3),
+            'args': ['hdu']
+        },
+        'sdut': {
+            'task': 'VJ.libs.tasks.problem_crawler',
+            'schedule': crontab(minute=0, hour=3),
+            'args': ['sdut']
+        },
+        'fzu': {
+            'task': 'VJ.libs.tasks.problem_crawler',
+            'schedule': crontab(minute=0, hour=3),
+            'args': ['fzu']
         },
     }
 
