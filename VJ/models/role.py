7b9bfb0540081e41fbdc5ea2167183bac368f097
nplus <junnplus@qq.com>
2015-07-27 23:27:55 +0800

    role permission

--- VJ/models/role.py
+++ VJ/models/role.py
@@ -2,9 +2,14 @@ from VJ import db
 from datetime import datetime
 
 
+class Permission(object):
+    ADMINISTER = 0x80
+
+
 class RoleModel(db.Document):
     name = db.StringField(max_length=80, unique=True)
     description = db.StringField(max_length=255)
+    permissions = db.IntField()
     created_at = db.DateTimeField(default=datetime.now, required=True)
 
     def __unicode__(self):
