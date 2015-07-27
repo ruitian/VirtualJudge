e08ce6fb86a4060d196d4f650cf674f8ff2c9f5e
nplus <junnplus@qq.com>
2015-07-27 23:14:33 +0800

    move config

---
+++ VJ/config.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+from celery.schedules import crontab
+
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    import sys
+    reload(sys)  # noqa
+    sys.setdefaultencoding('utf-8')
+
+    SECRET_KEY = (
+        os.environ.get('SECRET_KEY') or
+        u'\x97\xfa%\xab\xd2\xc2\xf8\xfc\xef\xaeTKDk\xc0\xe1//($\xc7\xc0'
+    )
+
+    CSRF_ENABLED = True
+
+    # pagination
+    PROBLEM_PER_PAGE = 20
+    CONTEST_PER_PAGE = 20
+    STATUS_PER_PAGE = 50
+    UPLOAD_FOLDER = BASE_DIR + '/app/uploads'
+    GRAVATAR_BASE_URL = 'http://gravatar.duoshuo.com/avatar/'
+
+    # mongodb
+    MONGODB_SETTINGS = {
+        'db': 'OJCC',
+        'username': '',
+        'password': '',
+        'host': '127.0.0.1',
+        'port': 27017
+    }
+
+    # email
+    MAIL_SERVER = 'smtp.sina.cn'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    VJ_MAIL_SUBJECT_PREFIX = '[VJ]'
+    VJ_MAIL_SENDER = os.environ.get('MAIL_USERNAME')
+    VJ_ADMIN = os.environ.get('VJ_ADMIN')
+
+    # redis
+    REDIS_URL = 'redis://%s:%s/%s' % (
+        os.environ.get('REDIS_HOST', 'localhost'),
+        os.environ.get('REDIS_PORT', '6379'),
+        os.environ.get('REDIS_DATABASE', '1'),
+    )
+
+    # celery
+    CELERY_BROKER_URL = 'redis://%s:%s' % (
+        os.environ.get('REDIS_HOST', 'localhost'),
+        os.environ.get('REDIS_PORT', '6379')
+    )
+    CELERY_BROKER_BACKEND = 'redis://%s:%s' % (
+        os.environ.get('REDIS_HOST', 'localhost'),
+        os.environ.get('REDIS_PORT', '6379')
+    )
+    CELERYBEAT_SCHEDULE = {
+        'test': {
+            'task': 'stepbystep.libs.tasks.test',
+            'schedule': crontab(minute='*/1'),
+            'args': (1, 2, 3)
+        },
+    }
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+
+
+class ProductionConfig(Config):
+    pass
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
