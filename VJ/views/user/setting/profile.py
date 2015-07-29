09505ed96cc3ab080aabba5147b74fcd1c481ffe
nplus <junnplus@qq.com>
2015-07-29 19:10:05 +0800

    move

--- VJ/views/user/setting/profile.py
+++ VJ/views/user/setting/profile.py
@@ -1,15 +1,15 @@
-from flask import (
+from flask import (  # noqa
     request,
     redirect,
     url_for,
     render_template
-)  # noqa
+)
 from flask.views import MethodView
 from flask.ext.login import login_user, login_required  # noqa
 
 from VJ.models import UserModel  # noqa
 
-from VJ.forms import UpdateProfileForm
+from VJ.forms import ProfileForm
 
 
 class ProfileView(MethodView):
@@ -18,20 +18,7 @@ class ProfileView(MethodView):
 
     @login_required
     def get(self):
-        return render_template(self.template)
-
-    @login_required
-    def post(self):
-        pass
-
-
-class UpdateProfileView(MethodView):
-
-    template = 'user/settings/updateprofile.html'
-
-    @login_required
-    def get(self):
-        form = UpdateProfileForm()
+        form = ProfileForm()
         return render_template(self.template, form=form)
 
     @login_required
