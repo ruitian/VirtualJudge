00ecda0b714bab8879aa1cd2174623e8680e136d
nplus <junnplus@qq.com>
2015-08-01 21:28:12 +0800

    submit

--- VJ/forms/submit.py
+++ VJ/forms/submit.py
@@ -5,6 +5,49 @@ from wtforms.validators import Required, ValidationError, Length
 from VJ.models import SolutionItem
 from VJ.models import ProblemItem
 
+lang = {
+    'poj': [
+        ('g++', 'G++'),
+        ('gcc', 'GCC'),
+        ('pascal', 'Pascal'),
+        ('java', 'Java'),
+        ('c++', 'C++'),
+        ('c', 'C'),
+        ('fortran', 'Fortran'),
+    ],
+    'hdu': [
+        ('g++', 'G++'),
+        ('gcc', 'GCC'),
+        ('pascal', 'Pascal'),
+        ('java', 'Java'),
+        ('c++', 'C++'),
+        ('c', 'C'),
+        ('c#', 'C#'),
+    ],
+    'sdut': [
+        ('gcc', 'GCC'),
+        ('g++', 'G++'),
+        ('java', 'Java'),
+        ('pascal', 'Pascal'),
+        ('go', 'Go'),
+        ('lua', 'Lua'),
+        ('dao', 'Dao'),
+        ('perl', 'Perl'),
+        ('ruby', 'Ruby'),
+        ('haskell', 'Haskell'),
+        ('python2', 'Python2'),
+        ('python3', 'Python3')
+    ],
+    'fzu': [
+        ('g++', 'G++'),
+        ('gcc', 'GCC'),
+        ('pascal', 'Pascal'),
+        ('java', 'Java'),
+        ('c++', 'C++'),
+        ('c', 'C'),
+    ]
+}
+
 
 class SubmitForm(Form):
     origin_oj = StringField('OJ', [Required()])
@@ -32,6 +75,12 @@ class SubmitForm(Form):
         validators=[Required()]
     )
     code = TextAreaField('Code', [Required(), Length(min=4)])
+
+    def __init__(self, origin_oj, problem_id, *args, **kwargs):
+        super(SubmitForm, self).__init__(*args, **kwargs)
+        self.origin_oj.data = origin_oj
+        self.problem_id.data = problem_id
+        self.language.choices = lang[origin_oj]
 
     def validate_origin_oj(self, field):
         if not ProblemItem.objects.filter(origin_oj=field.data):
