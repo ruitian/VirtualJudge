724cb8f2df5b234af8b910cb734fa33ff3026fed
nplus <junnplus@qq.com>
2015-08-01 09:37:24 +0800

    pep8

--- VJ/views/solution/__init__.py
+++ VJ/views/solution/__init__.py
@@ -7,7 +7,7 @@ bp_solution = Blueprint('solution', __name__)
 
 bp_solution.add_url_rule(
     '',
-    endpoint = 'list',
-    view_func = SolutionListView.as_view('list'),
-    methods = ['get', 'post']
+    endpoint='list',
+    view_func=SolutionListView.as_view('list'),
+    methods=['get', 'post']
 )
